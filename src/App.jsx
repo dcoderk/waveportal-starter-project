804f13f97b59c65ceb6ddb6a25ac8cbaab47ae3a
Jay Ramilo <webmarket2k@gmail.com>
2022-09-11 17:00:18 +0000

    adding wave function to interact with smart contract

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,23 @@
 import React, { useEffect, useState } from "react";
+import { ethers } from "ethers";
 import './App.css';
+import abi from "./utils/WavePortal.json";
 
 const App = () => {
   /*
   * Just a state variable we use to store ouruser's public wallet
   */
   const [currentAccount, setCurrentAccount] = useState("");
+
+  /*
+  * Create a variable here that holds the contract address after you deploy!
+  */
+  const contractAddress = "0x369694262D45ad1FEF7bfF44f5832739Ac20de71";
+
+  /*
+  * Create a variable here that references the abi content;
+  */
+  const contractABI = abi.abi;
   
   const checkIfWalletIsConnected = async () => {
     /*
@@ -59,6 +71,40 @@ const App = () => {
   }
 
   /*
+  * To retrieve total number of waves
+  */
+  const wave = async () => {
+    try {
+      const { ethereum } = window;
+
+      if (ethereum) {
+        const provider = new ethers.providers.Web3Provider(ethereum);
+        const signer = provider.getSigner();
+        const wavePortalContract = new ethers.Contract(contractAddress, contractABI, signer);
+
+        let count = await wavePortalContract.getTotalWaves();
+        console.log("Retrieved total wave count ...", count.toNumber());
+
+        /*
+        * Execute the actual wave from your smart contract
+        */
+        const waveTxn = await wavePortalContract.wave();
+        console.log("Mining...", waveTxn.hash);
+
+        await waveTxn.wait();
+        console.log("Mined --", waveTxn.hash);
+
+        count = await wavePortalContract.getTotalWaves();
+        console.log("Retrieved total wave count...", count.toNumber());
+      } else {
+        console.log("Ethereum object doesn't exist!");
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  /*
   * This runs our function when the page loads
   */
   useEffect(() => {
@@ -76,7 +122,7 @@ const App = () => {
           I am Jay and I worked on Self-driving cars so that's pretty cool right? Connect your Ethereum and wave at me!
         </div>
 
-        <button className="waveButton" onClick={null}>
+        <button className="waveButton" onClick={wave}>
           Wave at Me
         </button>
 
